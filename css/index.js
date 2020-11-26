395fb05ca7ac75a92f44fb74a5192967e9becf6a
Takuya Fukuju <chalkygames123@gmail.com>
2020-11-26 18:52:08 +0900

    fix: stop extending the stylelint-config-prettier
    BREAKING CHANGE: The stylelint-config-prettier is no longer extended to make it optional and allow
    users to extend additional configs.

--- css/index.js
+++ css/index.js
@@ -1,9 +1,5 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-recess-order',
-    'stylelint-config-prettier',
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   plugins: ['stylelint-order'],
   rules: {
     'alpha-value-notation': 'number',
