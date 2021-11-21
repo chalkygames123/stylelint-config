141f20cf28dff5b01347a8367e0cd9474bd2ffc5
Takuya Fukuju <chalkygames123@gmail.com>
2021-11-22 02:59:43 +0900

    fix(scss): change option for `scss/no-duplicate-dollar-variables` rule
    from:
    
    ```js
    [
      true,
      {
        ignoreInsideAtRules: ["if", "mixin"],
      },
    ];
    ```
    
    to:
    
    ```js
    true;
    ```

--- scss/index.js
+++ scss/index.js
@@ -13,12 +13,7 @@ module.exports = {
     'scss/dimension-no-non-numeric-values': true,
     'scss/function-color-relative': true,
     'scss/map-keys-quotes': 'always',
-    'scss/no-duplicate-dollar-variables': [
-      true,
-      {
-        ignoreInsideAtRules: ['if', 'mixin'],
-      },
-    ],
+    'scss/no-duplicate-dollar-variables': true,
     'scss/partial-no-import': true,
     'scss/selector-no-redundant-nesting-selector': true,
     'scss/selector-no-union-class-name': true,
