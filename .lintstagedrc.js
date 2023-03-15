32aafa6c4b6488b7dc8fd8eee23b753395204c13
Takuya Fukuju <chalkygames123@gmail.com>
2023-03-15 16:05:35 +0900

    chore(lint-staged): ensure `package.json` and `package-lock.json` are always in sync

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,8 @@
 module.exports = {
 	'*.{js,mjs,cjs,jsx}': ['eslint --fix', 'prettier --check --write'],
 	'!*.{js,mjs,cjs,jsx}': 'prettier --check --write --ignore-unknown',
+	'package.json': () => [
+		'npm install --package-lock-only',
+		'git add package-lock.json',
+	],
 };
